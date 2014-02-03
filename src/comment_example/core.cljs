a87c364b1a442496410da9c9146ef15aa7262242
J. David Smith <emallson@archlinux.us>
2014-02-03 14:46:18 -0500

    Added some styyyyyle

--- src/comment_example/core.cljs
+++ src/comment_example/core.cljs
@@ -6,20 +6,21 @@
 (enable-console-print!)
 
 (def app-state
-  (atom {:comments [{:name "David" :content "Test"}
-                    {:name "David" :content "Test 2"}]}))
+  (atom {:comments [{:name "ClojureScript" :content "ClojureScript gets translated into JavaScript!"}
+                    {:name "Om and Sablono" :content "Om is a wrapper over Facebook's React.js. Sablono gives a nice HTML generation macro."}]}))
 
 (defn comment-view [comment owner]
   "Renders a single comment view based on the input `comment`"
   (om/component
    (html [:div 
-          [:h3 {:class "username"} (:name comment)]
-          [:div (:content comment)]])))
+          [:h3 {:class "comment-title"} (:name comment)]
+          [:div {:class "comment-content"} (:content comment)]])))
 
 (defn comment-list-view [comments]
   "Constructs a list containing all of the comments"
   (om/component
-   (html [:div (om/build-all comment-view comments {:key :id})])))
+   (html [:div {:class "comment-list"}
+          (om/build-all comment-view comments {:key :id})])))
 
 (defn update-state! [event owner key]
   (om/set-state! owner key (.. event -target -value)))
@@ -48,8 +49,9 @@
        :content ""})
     om/IRenderState
     (render-state [this state]
-      (html [:div
+      (html [:div {:class "add-comment"}
              [:input {:type "text"
+                      :placeholder "Title"
                       :value (:name state)
                       :on-change #(update-state! % owner :name)}]
              [:textarea {:value (:content state)
