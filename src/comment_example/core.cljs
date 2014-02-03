25a27d4dc4fbcf3a3f1a928bf307eca039f8c0b5
J. David Smith <emallson@archlinux.us>
2014-02-03 11:37:28 -0500

    Initial commit.

---
+++ src/comment_example/core.cljs
@@ -0,0 +1,5 @@
+(ns emallson.comment-example.core)
+
+(enable-console-print!)
+
+(println "Hello world!")
---
+++ test/comment-example/core_test.cljs
@@ -0,0 +1,4 @@
+(ns emallson.comment-example.core-test
+  (:require-mcaros [cemerick.cljs.test
+                    :refer (is deftest with-test run-tests testing test-var)])
+  (:require [cemerick.cljs.test :as t]))
